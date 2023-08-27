9795caee2258cc6a299bb29b14f658c17f032a9a
Stephen Titcombe <stitcombe@gmail.com>
2023-08-27 18:01:03 -0500

    chore: type completion

--- src/components/Feedback/Submitted.tsx
+++ src/components/Feedback/Submitted.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Button, PopoverBody, Heading } from '@chakra-ui/react';
 
-export default function Submitted({ onStartOver }) {
+export default function Submitted({
+  onStartOver,
+}: {
+  onStartOver: React.MouseEventHandler<HTMLButtonElement>;
+}): JSX.Element {
   return (
     <PopoverBody>
       <Heading as="h2" size="sm" margin={2}>
         Thanks for submitting your feedback!
       </Heading>
-      <Button onClick={() => onStartOver()}>Start Over</Button>
+      <Button onClick={onStartOver}>Start Over</Button>
     </PopoverBody>
   );
 }
