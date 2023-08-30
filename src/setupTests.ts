2e19530e3297a9c5ca47a61f7d165589a4dfd1bc
Stephen Titcombe <stitcombe@gmail.com>
2023-08-30 09:31:44 -0500

    chore: config

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom';
 import { afterEach, expect } from 'vitest';
-import matchers from '@testing-library/jest-dom/matchers';
+// import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
 
 // extends vitest expect method with methods from react-testing-lbirary
-expect.extend(matchers);
+// expect.extend(matchers);
 
 // clears jsdom after each test
 afterEach(() => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import { configDefaults } from 'vitest/dist/config.js';
+import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
